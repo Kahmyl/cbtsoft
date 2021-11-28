18060afaa87a02b432564ddbc1572aa33e03f1c7
tsherrifdeen <43505184+tsherrifdeen@users.noreply.github.com>
2021-11-28 18:48:04 +0100

    Created Login Page
    Linked css files, created login page, tested responsiveness

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,14 @@
 <template>
-  <nuxt-link to="/Main">Main</nuxt-link>
+<div class="login-signup">
+  <h2 class="centered-text">Login</h2>
+  <form action="">
+    <input type="text" id="username" class="login-input" placeholder="Username or Email Address">
+    <input type="password" id="password" class="login-input" placeholder="Password">
+    <input type="button" value="Login" class="submit-btn">
+  </form>
+  <!-- <a href="" class="centered-link">Forgot password</a> -->
+</div>
+
 </template>
 
 <script>
